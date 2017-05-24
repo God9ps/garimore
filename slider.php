5928889f1a8c80718ab46e528f64438b755c9958
paulo <pauloamserrano@gmail.com>
2017-05-24 11:58:30 +0100

    slider principal

--- slider.php
+++ slider.php
@@ -14,10 +14,9 @@
                         <!--Slide Content-->
                         <div class="outer-box">
                             <div class="upper-content">
-                                <h2><strong>Bar</strong>reiro</h2>
+                                <h2><strong>Barreiro</strong></h2>
                                 <div class="text">Rua não sei quantas 45, <br>Barreiro, Portugal</div>
                                 <ul class="info-list clearfix">
-                                    <li><strong>82m2</strong> Area</li>
                                     <li><strong>2</strong> Quartos</li>
                                     <li><strong>1</strong> Casa de Banho</li>
                                     <li><strong>1</strong> Cozinha</li>
@@ -37,20 +36,19 @@
                 <li data-transition="fade" data-slotamount="1" data-masterspeed="1000" data-thumb="images/main-slider/1.jpg" data-saveperformance="off" data-title="A casa dos seus sonhos">
                     <img src="images/main-slider/2.jpg" alt="" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
 
-                    <div class="tp-caption sfl sfb tp-resizeme" data-x="left" data-hoffset="15" data-y="center" data-voffset="20" data-speed="1500" data-start="0" data-easing="easeOutExpo" data-splitin="none" data-splitout="none" data-elementdelay="0.01" data-endelementdelay="0.3" data-endspeed="1200" data-endeasing="Power4.easeIn">
+                    <div class="tp-caption sfl sfb tp-resizeme" data-x="center" data-hoffset="15" data-y="center" data-voffset="20" data-speed="1500" data-start="0" data-easing="easeOutExpo" data-splitin="none" data-splitout="none" data-elementdelay="0.01" data-endelementdelay="0.3" data-endspeed="1200" data-endeasing="Power4.easeIn">
 
 
                         <!--Slide Content-->
                         <div class="outer-box">
                             <div class="upper-content">
-                                <h2><strong>Bar</strong>reiro</h2>
+                                <h2><strong>Barreiro</strong></h2>
                                 <div class="text">Rua não sei quantas 45, <br>Barreiro, Portugal</div>
                                 <ul class="info-list clearfix">
-                                    <li><strong>82m2</strong> Area</li>
-                                    <li><strong>2</strong> Quartos</li>
-                                    <li><strong>1</strong> Casa de Banho</li>
-                                    <li><strong>1</strong> Cozinha</li>
-                                    <li><strong>1</strong> Sala</li>
+                                    <li><strong>15m2</strong> Quarto</li>
+                                    <li><strong>11m2</strong> Quarto</li>
+                                    <li><strong>13m2</strong> Cozinha</li>
+                                    <li><strong>21m2</strong> Sala</li>
                                 </ul>
                             </div>
                             <div class="lower-info">
@@ -64,9 +62,9 @@
                 </li>
 
                 <li data-transition="fade" data-slotamount="1" data-masterspeed="1000" data-thumb="images/main-slider/1.jpg" data-saveperformance="off" data-title="A casa dos seus sonhos">
-                    <img src="images/main-slider/1.jpg" alt="" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
+                    <img src="images/main-slider/3.jpg" alt="" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
 
-                    <div class="tp-caption sfl sfb tp-resizeme" data-x="left" data-hoffset="15" data-y="center" data-voffset="20" data-speed="1500" data-start="0" data-easing="easeOutExpo" data-splitin="none" data-splitout="none" data-elementdelay="0.01" data-endelementdelay="0.3" data-endspeed="1200" data-endeasing="Power4.easeIn">
+                    <div class="tp-caption sfl sfb tp-resizeme" data-x="right" data-hoffset="15" data-y="center" data-voffset="20" data-speed="1500" data-start="0" data-easing="easeOutExpo" data-splitin="none" data-splitout="none" data-elementdelay="0.01" data-endelementdelay="0.3" data-endspeed="1200" data-endeasing="Power4.easeIn">
 
                         <!--Slide Content-->
                         <div class="outer-box">
@@ -74,11 +72,7 @@
                                 <h2><strong>Bar</strong>reiro</h2>
                                 <div class="text">Rua não sei quantas 45, <br>Barreiro, Portugal</div>
                                 <ul class="info-list clearfix">
-                                    <li><strong>82m2</strong> Area</li>
-                                    <li><strong>2</strong> Quartos</li>
-                                    <li><strong>1</strong> Casa de Banho</li>
-                                    <li><strong>1</strong> Cozinha</li>
-                                    <li><strong>1</strong> Sala</li>
+                                    <li><strong>82m2</strong> Area Total</li>
                                 </ul>
                             </div>
                             <div class="lower-info">
@@ -92,21 +86,20 @@
                 </li>
 
                 <li data-transition="fade" data-slotamount="1" data-masterspeed="1000" data-thumb="images/main-slider/1.jpg" data-saveperformance="off" data-title="A casa dos seus sonhos">
-                    <img src="images/main-slider/2.jpg" alt="" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
+                    <img src="images/main-slider/4.jpg" alt="" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
 
-                    <div class="tp-caption sfl sfb tp-resizeme" data-x="left" data-hoffset="15" data-y="center" data-voffset="20" data-speed="1500" data-start="0" data-easing="easeOutExpo" data-splitin="none" data-splitout="none" data-elementdelay="0.01" data-endelementdelay="0.3" data-endspeed="1200" data-endeasing="Power4.easeIn">
+                    <div class="tp-caption sfl sfb tp-resizeme" data-x="center" data-hoffset="15" data-y="center" data-voffset="20" data-speed="1500" data-start="0" data-easing="easeOutExpo" data-splitin="none" data-splitout="none" data-elementdelay="0.01" data-endelementdelay="0.3" data-endspeed="1200" data-endeasing="Power4.easeIn">
 
                         <!--Slide Content-->
                         <div class="outer-box">
                             <div class="upper-content">
-                                <h2><strong>Bar</strong>reiro</h2>
+                                <h2><strong>Serviços próximos</strong></h2>
                                 <div class="text">Rua não sei quantas 45, <br>Barreiro, Portugal</div>
                                 <ul class="info-list clearfix">
-                                    <li><strong>82m2</strong> Area</li>
-                                    <li><strong>2</strong> Quartos</li>
-                                    <li><strong>1</strong> Casa de Banho</li>
-                                    <li><strong>1</strong> Cozinha</li>
-                                    <li><strong>1</strong> Sala</li>
+                                    <li><strong>2</strong> Supermercados</li>
+                                    <li><strong>4</strong> Restaurantes/Cafés</li>
+                                    <li><strong>2</strong> Paragens Autocarros</li>
+                                    <li><strong>1</strong> Estação Comboio</li>
                                 </ul>
                             </div>
                             <div class="lower-info">
